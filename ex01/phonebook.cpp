22ce336a095da424727346da52ed0b3ab98f8e56
Mark Razem <mrazem@3-D-10.42heilbronn.de>
2025-12-14 19:06:49 +0100

    mmmmhm

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,7 +1,19 @@
 #include "phonebook.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
+    Contact c1("mark", "razem", "asd", "123123", "youwish");
 
+    if (ac != 2)
+        return(std::cout << "no bueno" << std::endl, 1);
+    std::string input = av[1];
+    if (input == "ADD")
+        std::cout << "ADD-BRANCH" << std::endl;
+    else if (input == "SEARCH")
+        std::cout << "SEARCH - Branch" << std::endl;
+    else if (input == "EXIT")
+        std::cout << "EXIT - Branch" << std::endl;
+
+    std::cout << c1.get_first_name() << std::endl;
     return (0);
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -1,3 +1,41 @@
 #ifndef PHONEBOOK_H
 #define PHONEBOOK_H
+
+# include <iostream>
+# include <string>
+
+// class PhoneBook
+// {
+//     private:
+
+//     public:
+// };
+
+class Contact
+{
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest_secret;
+
+
+
+    public:
+        Contact();
+        Contact(std::string, std::string, std::string, std::string, std::string);
+        //setters
+        void set_first_name(std::string first_name);
+        void set_last_name(std::string last_name);
+        void set_nickname(std::string nickname);
+        void set_phone_number(std::string phone_number);
+        void set_darkest_secret(std::string darkest_secret);
+        //getters
+        std::string get_first_name(){return first_name;}
+        std::string get_last_name(){return last_name;}
+        std::string get_nickname(){return nickname;}
+        std::string get_phone_number(){return phone_number;}
+        std::string get_darkest_secret(){return darkest_secret;}
+};
+
 #endif
