b154e80b6ea15a320a7862ec89b4a9ec0ef659e2
Mark Razem <mrazem@3-D-10.42heilbronn.de>
2025-12-15 18:53:59 +0100

    cleanup I guess

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,22 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrazem <mrazem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 18:26:12 by mrazem            #+#    #+#             */
+/*   Updated: 2025/12/15 18:26:15 by mrazem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-#include <string>
+#include <string.h>
 
 int main(int ac, char **av)
 {
     if (ac < 2)
-    {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return(0);
-    }
     else
     {
-        int i = 0;
-        while (i + 1 < ac)
+        int i = 1;
+        while (i < ac)
         {
-            std::string str = av[i + 1];
             size_t j = 0;
+            std::string str = av[i];
             while (j < str.length())
-                std::cout << (char)std::toupper(str[j++]);
+                std::cout << (char)toupper(str[j++]);
             i++;
         }
         std::cout << std::endl;
